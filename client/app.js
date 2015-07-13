23375b5af1e4f54c6324e22ece2135e198571484
rogueSnake <eugorekans@gmail.com>
2015-07-12 19:04:13 -0500

    Added timeManager.js, which creates timers.

--- client/app.js
+++ client/app.js
@@ -1,7 +1,31 @@
 var angular = require('angular'),
+  timeManager = require('./timeManager'),
   app = angular.module('app', []);
 
-app.controller('mainController', ['$scope', function ($scope) {
-  $scope.message = "Not really a chess timer yet.";
+playerTimer = timeManager.makePlayerTimer();
+menuTimer = timeManager.makeMenuTimer(); 
+
+app.controller('mainCtrl', ['$scope', function ($scope) {
+  $scope.startTimeMinutes = 5;
+  $scope.startTimeHours = 0;
+  $scope.message = $scope.startTime;
+  $scope.addMinute = function () {
+    if ($scope.startTimeMinutes < 59) {$scope.startTimeMinutes += 1;}
+    else {
+      $scope.startTimeHours += 1;
+      $scope.startTimeMinutes = 0;
+    }
+  };
+  $scope.removeMinute = function () {
+
+    if ($scope.startTimeMinutes > 0){
+      $scope.startTimeMinutes -= 1;
+    }
+
+    else if ($scope.startTimeMinutes === 0 && $scope.startTimeHours > 0) {
+      $scope.startTimeHours -= 1;
+      $scope.startTimeMinutes = 59;
+    }
+  };  
 }]);
 
--- client/bundle.js
+++ client/bundle.js
@@ -28371,11 +28371,186 @@ module.exports = angular;
 
 },{"./angular":1}],3:[function(require,module,exports){
 var angular = require('angular'),
+  timeManager = require('./timeManager'),
   app = angular.module('app', []);
 
-app.controller('mainController', ['$scope', function ($scope) {
-  $scope.message = "Not really a chess timer yet.";
+playerTimer = timeManager.makePlayerTimer();
+menuTimer = timeManager.makeMenuTimer(); 
+
+app.controller('mainCtrl', ['$scope', function ($scope) {
+  $scope.startTimeMinutes = 5;
+  $scope.startTimeHours = 0;
+  $scope.message = $scope.startTime;
+  $scope.addMinute = function () {
+    if ($scope.startTimeMinutes < 59) {$scope.startTimeMinutes += 1;}
+    else {
+      $scope.startTimeHours += 1;
+      $scope.startTimeMinutes = 0;
+    }
+  };
+  $scope.removeMinute = function () {
+
+    if ($scope.startTimeMinutes > 0){
+      $scope.startTimeMinutes -= 1;
+    }
+
+    else if ($scope.startTimeMinutes === 0 && $scope.startTimeHours > 0) {
+      $scope.startTimeHours -= 1;
+      $scope.startTimeMinutes = 59;
+    }
+  };  
 }]);
 
 
-},{"angular":2}]},{},[3]);
+},{"./timeManager":4,"angular":2}],4:[function(require,module,exports){
+var protoTimer = {
+  onesOfMinutes : 0,
+  tensOfMinutes : 0,
+  onesOfHours : 0,
+
+  setTime : function (time) {
+    this.onesOfMinutes = Number(time.slice(3, 4));
+    this.tensOfMinutes = Number(time.slice(2, 3));
+    this.onesOfHours = Number(time.slice(0, 1));
+    return this.getTime();
+  },
+
+  getTime : function () {
+    return this.onesOfHours + ":" + this.tensOfMinutes + this.onesOfMinutes;
+  },
+
+  removeOneMinute : function () {
+
+    if (this.onesOfMinutes > 0) {
+      this.onesOfMinutes -= 1;
+    }
+
+    else if (this.tensOfMinutes > 0) {
+      this.onesOfMinutes = 9;
+      this.tensOfMinutes -= 1;
+    }
+
+    else if (this.onesOfHours > 0) {
+      this.onesOfMinutes = 9;
+      this.tensOfMinutes = 5;
+      this.onesOfHours -= 1;
+    }
+    return this.getTime();
+  },
+
+  removeTenMinutes : function () {
+
+    if (this.tensOfMinutes > 0) {
+      this.tensOfMinutes -= 1;
+    }
+
+    else if (this.onesOfHours > 0) {
+      this.tensOfMinutes = 5;
+      this.onesOfHours -= 1;
+    }
+    return this.getTime();
+  },
+
+  removeOneHour : function () {
+
+    if (this.onesOfHours > 0) {
+      this.onesOfHours -= 1;
+    }
+    return this.getTime();
+  }
+};
+
+var menuTimer = Object.create(protoTimer);
+
+menuTimer.addOneMinute = function () {
+
+  if (this.onesOfMinutes < 9) {
+    this.onesOfMinutes += 1;
+  }
+
+  else if (this.tensOfMinutes < 5) {
+    this.onesOfMinutes = 0;
+    this.tensOfMinutes += 1;
+  }
+
+  else if (this.onesOfHours < 9) {
+    this.onesOfHours += 1;
+  }
+
+  // If time is at 9:59 or greater, let timer keep counting to 9:99.
+  else if (this.tensOfMinutes < 9) {
+    this.onesOfMinutes = 0;
+    this.tensOfMinutes += 1;
+  }
+  return this.getTime();
+};
+
+menuTimer.addTenMinutes = function () {
+
+  if (this.tensOfMinutes < 5) {
+    this.tensOfMinutes += 1;
+  }
+
+  else if (this.onesOfHours < 9) {
+    this.tensOfMinutes = 0;
+    this.onesOfHours += 1;
+  }
+
+  else if (this.tensOfMinutes < 9) {
+    this.tensOfMinutes += 1;
+  }
+  return this.getTime();
+};
+
+menuTimer.addOneHour = function () {
+
+  if (this.onesOfHours < 9) {
+    this.onesOfHours += 1;
+  }
+  return this.getTime();
+};
+
+var playerTimer = Object.create(protoTimer);
+
+playerTimer.onesOfSeconds = 0;
+playerTimer.tensOfSeconds = 0;
+
+playerTimer.removeOneSecond = function () {
+
+  if (this.onesOfSeconds > 0) {
+    this.onesOfSeconds -= 1;
+  }
+
+  else if (this.tensOfSeconds > 0) {
+    this.onesOfSeconds = 9;
+    this.tensOfSeconds -= 1;
+  }
+
+  else if (this.getTime() > "0:00:00") {
+    this.onesOfSeconds = 9;
+    this.tensOfSeconds = 5;
+    this.removeOneMinute();
+  }
+  return this.getTime();
+};
+
+playerTimer.getTime = function () {
+  return this.onesOfHours + ":" + this.tensOfMinutes + this.onesOfMinutes + 
+      ":" + this.tensOfSeconds + this.onesOfSeconds;
+};
+
+var makeMenuTimer = function () {
+  return Object.create(menuTimer);
+};
+
+var makePlayerTimer = function () {
+  return Object.create(playerTimer);
+};
+
+module.exports = {
+  makeMenuTimer : makeMenuTimer,
+  makePlayerTimer : makePlayerTimer
+};
+
+
+},{}]},{},[3]);
